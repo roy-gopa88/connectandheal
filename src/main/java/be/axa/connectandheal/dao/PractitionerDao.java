bc76289622de450547a48ea12648054ba01edb74
gopa1988 <goparoy1988@gmail.com>
2020-12-06 22:24:41 +0100

    getPractitioner - read from json file

--- src/main/java/be/axa/connectandheal/dao/PractitionerDao.java
+++ src/main/java/be/axa/connectandheal/dao/PractitionerDao.java
@@ -1,8 +1,16 @@
 package be.axa.connectandheal.dao;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Repository;
 import be.axa.connectandheal.model.Doctor;
 
@@ -11,17 +19,50 @@ public class PractitionerDao {
 	
     public List<Doctor> getPractitioner(String latitude, String longitude, String radius, String specialization) 
     {
-    	List<Doctor> doctorList = new ArrayList<Doctor>();
-    	Doctor doctor = new Doctor();
-    	doctor.setFirstName("AAA");
-    	doctor.setId("1");
-    	doctor.setIdCard("QBC012");
-    	doctor.setLastName("BBB");
-    	doctorList.add(doctor);
+    	
+    	//JSON parser object to parse read file
+        JSONParser jsonParser = new JSONParser();
+        JSONArray doctorList = null;
+        try (FileReader reader = new FileReader(new File("").getAbsolutePath() +"//src//main//resources//doctors.json"))
+        {
+            //Read JSON file
+            Object obj = jsonParser.parse(reader);
+ 
+            doctorList = (JSONArray) obj;
+            System.out.println(doctorList);
+             
+            //Iterate over employee array
+            doctorList.forEach( emp -> parseEmployeeObject( (JSONObject) emp ) );
+ 
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    	
 		return doctorList;
 
-    	
-    	
+    }
+    
+    private Doctor parseEmployeeObject(JSONObject employee) 
+    {
+    	Doctor doctor = new Doctor();
+        String id = (String) employee.get("id");    
+        doctor.setId(id);
+         
+        String firstName = (String) employee.get("firstName");  
+        doctor.setFirstName(firstName);;
+         
+        String lastName = (String) employee.get("lastName");    
+        doctor.setLastName(lastName);
+        
+        String idCard = (String) employee.get("idCard");    
+        doctor.setIdCard(idCard);
+		return doctor;
+        
+        
     }
      
 }
