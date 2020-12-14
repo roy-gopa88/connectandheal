f2165ce64d26ca2c2df07509ba3814e5dd1f3097
DGRO861 <DGRO861@10VM00807.axa-be.intraxa>
2020-12-14 12:19:49 +0100

    CrossOrigin added

--- src/main/java/be/axa/connectandheal/controller/AppointmentController.java
+++ src/main/java/be/axa/connectandheal/controller/AppointmentController.java
@@ -4,6 +4,7 @@ import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import be.axa.connectandheal.dao.AppointmentDao;
 import be.axa.connectandheal.model.Appointment;
 import be.axa.connectandheal.model.Appointments;
 
+@CrossOrigin(origins = "*", allowedHeaders = "")
 @RestController
 public class AppointmentController {
 	@Autowired
--- src/main/java/be/axa/connectandheal/controller/PractitionerController.java
+++ src/main/java/be/axa/connectandheal/controller/PractitionerController.java
@@ -3,13 +3,14 @@ package be.axa.connectandheal.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import be.axa.connectandheal.dao.PractitionerDao;
 import be.axa.connectandheal.model.Doctor;
 
-
+@CrossOrigin(origins = "*", allowedHeaders = "")
 @RestController
 public class PractitionerController {
 	@Autowired
